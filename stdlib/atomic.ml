e9330b83f817ee13f3b158eecf098fd415e5c676
Sumit Padhiyar <padhiyarsumit@gmail.com>
2021-02-03 18:32:52 +0530

    Added context switch in atomic module

--- stdlib/atomic.ml
+++ stdlib/atomic.ml
@@ -1,10 +1,15 @@
 type 'a t
 
 external make : 'a -> 'a t = "%makemutable"
-external get : 'a t -> 'a = "%atomic_load"
-external exchange : 'a t -> 'a -> 'a = "%atomic_exchange"
-external compare_and_set : 'a t -> 'a -> 'a -> bool = "%atomic_cas"
-external fetch_and_add : int t -> int -> int = "%atomic_fetch_add"
+external _get : 'a t -> 'a = "%atomic_load"
+external _exchange : 'a t -> 'a -> 'a = "%atomic_exchange"
+external _compare_and_set : 'a t -> 'a -> 'a -> bool = "%atomic_cas"
+external _fetch_and_add : int t -> int -> int = "%atomic_fetch_add"
+
+let get r = Scheduler.context_switch (); _get r
+let exchange r x = Scheduler.context_switch (); _exchange r x
+let compare_and_set r x y = Scheduler.context_switch (); _compare_and_set r x y
+let fetch_and_add r x = Scheduler.context_switch (); _fetch_and_add r x
 
 let set r x =
   exchange r x |> ignore
--- stdlib/atomic.mli
+++ stdlib/atomic.mli
@@ -1,10 +1,15 @@
 type 'a t
 
 external make : 'a -> 'a t = "%makemutable"
-external get : 'a t -> 'a = "%atomic_load"
-external exchange : 'a t -> 'a -> 'a = "%atomic_exchange"
-external compare_and_set : 'a t -> 'a -> 'a -> bool = "%atomic_cas"
-external fetch_and_add : int t -> int -> int = "%atomic_fetch_add"
+(* external get : 'a t -> 'a = "%atomic_load" *)
+(* external exchange : 'a t -> 'a -> 'a = "%atomic_exchange" *)
+(* external compare_and_set : 'a t -> 'a -> 'a -> bool = "%atomic_cas" *)
+(* external fetch_and_add : int t -> int -> int = "%atomic_fetch_add" *)
+
+val get : 'a t -> 'a
+val exchange : 'a t -> 'a -> 'a
+val compare_and_set : 'a t -> 'a -> 'a -> bool
+val fetch_and_add : int t -> int -> int
 
 val set : 'a t -> 'a -> unit
 val incr : int t -> unit
