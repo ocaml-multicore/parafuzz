caa39aa6e93abb33de82706a12ba9e2d2fd29516
Sumit Padhiyar <padhiyarsumit@gmail.com>
2021-02-12 14:44:42 +0530

    Added individual AFL instrumentation in atomic API

--- stdlib/atomic.ml
+++ stdlib/atomic.ml
@@ -6,10 +6,25 @@ external _exchange : 'a t -> 'a -> 'a = "%atomic_exchange"
 external _compare_and_set : 'a t -> 'a -> 'a -> bool = "%atomic_cas"
 external _fetch_and_add : int t -> int -> int = "%atomic_fetch_add"
 
-let get r = Scheduler.context_switch (); _get r
-let exchange r x = Scheduler.context_switch (); _exchange r x
-let compare_and_set r x y = Scheduler.context_switch (); _compare_and_set r x y
-let fetch_and_add r x = Scheduler.context_switch (); _fetch_and_add r x
+let get r = 
+    Scheduler.context_switch ();
+    let _ = if true then () else () in
+    _get r
+
+let exchange r x = 
+    Scheduler.context_switch ();
+    let _ = if true then () else () in
+    _exchange r x
+
+let compare_and_set r x y = 
+    Scheduler.context_switch (); 
+    let _ = if true then () else () in
+    _compare_and_set r x y
+
+let fetch_and_add r x = 
+    Scheduler.context_switch ();
+    let _ = if true then () else () in
+    _fetch_and_add r x
 
 let set r x =
   exchange r x |> ignore
