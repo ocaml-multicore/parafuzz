7e41478fc3eb9804b56b3b62ebaeb0e2b374852e
Sumit Padhiyar <padhiyarsumit@gmail.com>
2021-02-19 17:24:08 +0530

    Added effects in scheduler

--- stdlib/scheduler.ml
+++ stdlib/scheduler.ml
@@ -17,16 +17,16 @@ let context_switch () = perform Context_switch
 
 let run afl_module main =
     let module M = (val afl_module : AFLQueue) in
-(*     let enqueue k v = M.enqueue (fun () -> continue k v) in *)
+    let enqueue k v = M.enqueue (fun () -> continue k v) in
     let dequeue () = M.dequeue () () in
-    let spawn f = 
+    let rec spawn f = 
         match f () with
         | () -> dequeue ()
-       (* | effect Context_switch k -> enqueue k (); dequeue () 
+        | effect Context_switch k -> enqueue k (); dequeue () 
         | effect (Fork f) k -> enqueue k (); spawn f
         | effect (Suspend f) k -> f k; dequeue ()
         | effect (Resume (k',v)) k ->
-                enqueue k' v; enqueue k (); dequeue ()*)
+                enqueue k' v; enqueue k (); dequeue ()
     in
     spawn main
 
