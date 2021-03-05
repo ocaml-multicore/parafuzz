b7dc3c9a353dbdc1aca5a944a9747c1282c48900
Adharsh Kamath <adharshkamathr@gmail.com>
2021-03-05 21:11:45 +0530

    Condition vars implementation changed, dependencies corrected

--- stdlib/domain.ml
+++ stdlib/domain.ml
@@ -56,6 +56,63 @@ module Sync = struct
   external poll : unit -> unit = "%poll"
 end
 
+module Mvar = struct
+
+  type 'a state = 
+  | Filled of 'a * (((unit Scheduler.cont) * 'a * int) Queue.t)
+  (* The first element in the pair represents the value stored.
+    The second element contains a queue of continuations that have `put`, that are waiting to be run *)
+
+  | Empty of ('a Scheduler.cont * int) Queue.t
+  (* This represents a queue of continuations that have `get`, waiting to be run. These will be run
+    once the MVar has some value. *)
+
+  type 'a t = 'a state ref
+
+  let make_empty () = ref (Empty(Queue.create ()))
+
+  let make v = ref (Filled(v, (Queue.create ())))
+
+  let get mvar = match (!mvar) with
+  | Empty (wait_q) -> Scheduler.suspend (fun (cont,id) -> Queue.push (cont,id) wait_q)
+  | Filled (value, wait_q) -> 
+    if Queue.is_empty wait_q then 
+      (mvar := Empty(Queue.create ()); value)
+    else 
+      (let (waiting_f, nvalue, id) = Queue.pop wait_q in
+      (mvar := Filled(nvalue, wait_q); 
+      Scheduler.resume (waiting_f, (), id);
+        value))    
+
+  let put v mvar = match (!mvar) with
+  | Empty (wait_q) -> 
+    if Queue.is_empty wait_q then 
+      mvar := Filled(v, Queue.create ()) 
+    else 
+      let (waiting_f,id) = Queue.pop wait_q in 
+      Scheduler.resume (waiting_f, v, id)
+  | Filled (_, wait_q) -> Scheduler.suspend (fun (cont,id) -> Queue.push (cont, v, id) wait_q)
+
+  let put_all v mvar = match (!mvar) with
+  | Empty (wait_q) -> 
+    if Queue.is_empty wait_q then 
+      mvar := Filled(v, Queue.create ()) 
+    else 
+      Queue.iter (fun (cont, id) -> Scheduler.resume (cont, v, id)) wait_q; 
+      Queue.clear wait_q; 
+      mvar := Empty(Queue.create ())
+  | Filled (_, wait_q) -> Scheduler.suspend (fun (cont,id) -> Queue.push (cont, v, id) wait_q)
+
+  let try_get mvar = match !mvar with
+  | Empty (_) -> None
+  | _ -> Some (get mvar)
+
+  let try_put v mvar = match !mvar with
+  | Empty (_) -> (put v mvar); true
+  | _ -> false
+
+end
+
 module Mutex = struct
 
   type t = int Mvar.t
@@ -69,7 +126,7 @@ module Mutex = struct
   let unlock mut = let id = (Scheduler.get_id ()) in
     match (Mvar.try_get mut) with
   | None -> raise LockNotHeld
-  | Some (thread_id) -> if id = thread_id then () else Mvar.put thread_id mut; raise LockNotHeld
+  | Some (thread_id) -> if id = thread_id then () else (Mvar.put thread_id mut; raise LockNotHeld)
 
   let try_lock mut = Mvar.try_put (Scheduler.get_id ()) mut
 
@@ -77,18 +134,26 @@ end
 
 module Condition = struct
 
-  type t = bool Mvar.t
+  exception LockNotHeld
 
-  let create () = Mvar.make_empty ()
+  type t = ((bool Mvar.t) * (Mutex.t ref))
 
-  let wait cond mutex = 
-    Mutex.unlock mutex;
+  let create mutex = (Mvar.make_empty (), (ref mutex))
+
+  let wait (cond, mutex) = 
+    Mutex.unlock !mutex;
     ignore (Mvar.get cond);
-    Mutex.lock mutex
+    Mutex.lock !mutex
 
-  let signal cond = Mvar.put true cond  
+  let signal (cond, mutex) = 
+    if Mutex.try_lock !mutex then
+      raise LockNotHeld
+    else Mvar.put true cond
 
-  let broadcast cond = Mvar.put_all true cond
+  let broadcast (cond, mutex) = 
+    if Mutex.try_lock !mutex then
+      raise LockNotHeld 
+    else Mvar.put_all true cond
 
 end
 
